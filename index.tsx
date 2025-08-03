c260eed8025bab51b86c2cbcb7e0cb68bbcf9ce1
RIBECHI <desafyo@gmail.com>
2025-08-03 02:57:03 -0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      fontFamily: "Arial"
+    }}>
+      <h1>AgroUs</h1>
+      <p>Bem-vindo à sua fazenda virtual!</p>
+    </div>
+  );
+}
